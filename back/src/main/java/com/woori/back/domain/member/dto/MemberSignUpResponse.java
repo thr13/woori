56ddf6debe586e02e278752013ab0960aac9ff67
sugwan <skhrnt2945@gmail.com>
2026-01-01 17:50:41 +0900

    feat: add Member DTO, Service

---
+++ back/src/main/java/com/woori/back/domain/member/dto/MemberSignUpResponse.java
@@ -0,0 +1,13 @@
+package com.woori.back.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberSignUpResponse {
+    private Long id;
+    private String email;
+}
