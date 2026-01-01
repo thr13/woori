39320485847b9e24099de3feb09d9790defa6fac
sugwan <skhrnt2945@gmail.com>
2026-01-01 17:50:01 +0900

    feat: add Member entity

---
+++ back/src/main/java/com/woori/back/domain/member/entity/Member.java
@@ -0,0 +1,39 @@
+package com.woori.back.domain.member.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 회원 식별번호
+
+    @Column(nullable = false, unique = true)
+    private String email; // 로그인 아이디 역할 겸 이메일
+
+    @Column(nullable = false)
+    private String password; // 비밀번호
+
+    @Column(nullable = false)
+    private String name; // 회원명
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role; // 회원 등급(손님, 사장, 관리자)
+
+    @Builder
+    private Member(String email, String password, String name, Role role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.role = role;
+    }
+}
