ed9e6e6c268b30e8392ec00df113dd63a71d6120
sugwan <skhrnt2945@gmail.com>
2026-01-02 20:20:44 +0900

    feat: jwt 에서 accessToken, refreshToken 발급 추가

---
+++ back/src/main/java/com/woori/back/domain/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.woori.back.domain.auth.repository;
+
+import com.woori.back.domain.auth.entity.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+}
