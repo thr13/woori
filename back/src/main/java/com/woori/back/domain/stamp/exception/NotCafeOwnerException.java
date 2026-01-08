22fefb76a13c7d2123c95872f041203cc48a21ea
sugwan <skhrnt2945@gmail.com>
2026-01-08 21:42:15 +0900

    fix: 스탬프 예외 이름 수정 및 추가

---
+++ back/src/main/java/com/woori/back/domain/stamp/exception/NotCafeOwnerException.java
@@ -0,0 +1,7 @@
+package com.woori.back.domain.stamp.exception;
+
+public class NotCafeOwnerException extends RuntimeException {
+  public NotCafeOwnerException(String message) {
+    super(message);
+  }
+}
